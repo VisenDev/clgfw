538f6ee51fcfecc78381538fbb4d90e03d478f38
Robert Burnett <dr.slate2@gmail.com>
2025-12-29 07:07:24 -0600

    make some more progress on the x11 backend

--- x11.lisp
+++ x11.lisp
@@ -1,29 +1,36 @@
 (eval-when (:load-toplevel :compile-toplevel :execute)
+  ;;; automatically load clx for convenience when testing in repl
   (unless (find-package 'xlib)
-    (ql:quickload "clx")))
+    (asdf:load-system "clx")))
+
 (defpackage #:clgfw/x11
   (:use #:cl)
   (:export #:init-window
            #:close-window
-           #:window-should-close
+           #:window-should-keeping-running
            #:begin-drawing
            #:end-drawing
-           #:draw-pixel))
+           #:draw-rectangle
+           #:get-mouse-x
+           #:get-mouse-y))
 (in-package #:clgfw/x11)
 
 (defclass state ()
-  ((black :accessor black)
+  ((window-should-keep-running  :accessor window-should-keep-running :initform t)
+   (wm-delete-atom :accessor wm-delete-atom)
+   (black :accessor black)
    (white :accessor white)
    (font :accessor font)
    (display :accessor display)
    (screen :accessor screen)
    (window :accessor window)
-   (gcontext :accessor gcontext)))
+   (gcontext :accessor gcontext)
+   (colormap :accessor colormap)))
 
 (defun init-window (width height title &aux state)
   (declare (ignorable width height title))
   (setf state (make-instance 'state))
-  (with-slots (black white font display screen window gcontext) state
+  (with-slots (black white font display screen window gcontext colormap) state
     (setf display (xlib:open-default-display))
     (setf screen (first (xlib:display-roots display)))
     (setf black (xlib:screen-black-pixel screen))
@@ -45,67 +52,132 @@
     (xlib::set-wm-protocols
      window
      '("WM_DELETE_WINDOW"))
-    
     (xlib:set-wm-properties
      window
      :name title
      :width width
      :height height
      )
-    ;; (xlib:change-window-attribute
-    ;;  display
-    ;;  my-window
-    ;;  :event-mask
-    ;;  (xlib:make-event-mask
-    ;;   :structure-notify :property-change))
     (setf gcontext (xlib:create-gcontext
                     :drawable window
                     :background black
                     :foreground white))
+    (setf (wm-delete-atom state) (xlib:intern-atom display "WM_DELETE_WINDOW"))
     (xlib:map-window window)
+    (setf colormap (xlib:screen-default-colormap screen))
+    
     (xlib:display-finish-output display)
     (xlib:display-force-output display)
     state)
   )
 
-(defun test-render (state)
-  (multiple-value-bind (x y) (xlib:query-pointer (window state))
-    (xlib:draw-rectangle (window state) (gcontext state) x y 100 100 (white state))
-    (xlib:display-force-output (display state))))
+(defun draw-rectangle (state &key x y width height r g b)
+  (let* ((hash (format nil "~a~a~a" r g b))
+         (colormap (colormap state))
+         (pixel (ignore-errors (xlib:lookup-color colormap hash))))
+    (unless pixel
+      (setf pixel (xlib:alloc-color colormap
+                                    (xlib:make-color 
+                                     :red (/ r 256)  
+                                     :green (/ g 256)
+                                     :blue (/ b 256)))))
+    (setf (xlib:gcontext-foreground (gcontext state)) pixel)
+    (xlib:draw-rectangle (window state) (gcontext state) x y width height t)))
 
+(defun get-mouse-x (state)
+  (multiple-value-bind (x) (xlib:query-pointer (window state)) x))
+
+(defun get-mouse-y (state)
+  (multiple-value-bind (x y) (xlib:query-pointer (window state))
+    (declare (ignore x))
+    y))
+
+(defun begin-drawing (state)
+  (declare (ignore state)))
+
+(defun end-drawing (state)
+  (xlib:display-force-output (display state))
+  (when (xlib:event-listen (display state))
+    (xlib:event-case ((display state))
+      (:client-message (type data)
+                       ;; TYPE is an atom
+                       ;; DATA is a vector of 32-bit values
+                       (when (and (eq type :wm_protocols)
+                                  (eq (aref data 0) (wm-delete-atom state)))
+                         (setf (window-should-keep-running state) nil)
+                         (return-from end-drawing)))
+      (:destroy-notify ()
+                       (setf (window-should-keep-running state) nil)
+                       (return-from end-drawing))
+      (t () t)))
+  )
 
 (defun close-window (win)
   (xlib:close-display (display win) :abort nil))
 
+
+
+;;; HELPER CONVENIENCES
+;;; These should probably be moved to a common file rather than in x11 specifically
+(defmacro with-window (name (width height title) &body body)
+  `(let ((,name (init-window ,width ,height ,title)))
+     (unwind-protect
+          (progn ,@body)
+       (close-window ,name))))
+
+(defmacro with-drawing (state &body body)
+  `(progn
+     (begin-drawing ,state)
+     (unwind-protect (progn ,@body)
+       (end-drawing ,state)))
+  )
+
+(defmacro while-running-with-drawing (state &body body)
+  `(loop :while (window-should-keep-running ,state)
+         :do (with-drawing ,state ,@body)))
+
+
+;;; EXAMPLE
 (defun test-main ()
-  (let* ((win (init-window 100 100 "foo"))
-         (display (display win))
-         (wm-delete (xlib:intern-atom display "WM_DELETE_WINDOW")))
-    (unwind-protect                                              
-         (loop :while t
-               :do
-                  (test-render win)
-                  (format t "frame~%")
-                  (xlib:event-case (display)
-                    (:client-message (type data)
-                                     ;; TYPE is an atom
-                                     ;; DATA is a vector of 32-bit values
-                                     (when (and (eq type :wm_protocols)
-                                                (= (aref data 0) wm-delete))
-                                       (format t "WM_DELETE_WINDOW received~%")
-                                       (return-from test-main)))
-                    (:destroy-notify ()
-                                     (format t "Attempted to quit!~%")
-                                     (return-from test-main))
-                    (t () t))
-                  (sleep 0.01)
-               )                                           
-      (close-window win))                                      
+  "Example main function"
+  (with-window state (100 100 "Hello")
+    (while-running-with-drawing state
+      (draw-rectangle state
+                      :x (get-mouse-x state) :y (get-mouse-y state)
+                      :width 10 :height 10
+                      :r 200 :g 100 :b 100))))
+
+
+;; (defun test-main ()
+;;   (let* ((win (init-window 100 100 "foo"))
+;;          (display (display win))
+;;          (wm-delete (xlib:intern-atom display "WM_DELETE_WINDOW")))
+;;     (unwind-protect                                              
+;;          (loop :while t
+;;                :do
+;;                   (test-render win)
+;;                   (format t "frame~%")
+;;                   (when (xlib:event-listen display)
+;;                     (xlib:event-case (display)
+;;                       (:client-message (type data)
+;;                                        ;; TYPE is an atom
+;;                                        ;; DATA is a vector of 32-bit values
+;;                                        (when (and (eq type :wm_protocols)
+;;                                                   (eq (elt data 0) wm-delete))
+;;                                          (format t "WM_DELETE_WINDOW received~%")
+;;                                          (return-from test-main)))
+;;                       (:destroy-notify ()
+;;                                        (format t "Attempted to quit!~%")
+;;                                        (return-from test-main))
+;;                       (t () t)))
+;;                   (sleep 0.01)
+;;                )                                           
+;;       (close-window win))                                      
     
-    ;; (loop
-    ;;   (test-render win))
-    ;; (close-window win)
-    ))
+;;     ;; (loop
+;;     ;;   (test-render win))
+;;     ;; (close-window win)
+;;     ))
     
 ;;     ;; (unwind-protect
 ;;     ;;      (loop :while 
