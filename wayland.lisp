d48c4cef1012783d1a1687e7ee18243e814febda
Robert Burnett <dr.slate2@gmail.com>
2026-01-03 12:19:38 -0600

    updated wayland.lisp to get some basic things working

--- wayland.lisp
+++ wayland.lisp
@@ -1,7 +1,3 @@
-;; (cl:defpackage #:clgfw/wayland
-;;   (:use #:cl #:wayflan #:wayflan-client #:wayflan-client.xdg-shell #:clgfw))
-;; (in-package #:clgfw/wayland)
-
 (in-package #:clgfw)
 
 
@@ -17,6 +13,15 @@
 ;;; This work isn't licensed yet
 ;;; See LICENSE for more details.
 
+(defclass render-buffer () (
+   (pool-data :accessor pool-data
+              :initarg :pool-data
+              :documentation "This is the actual memory buffer that can be aref'd to write pixels")
+   (buffer :accessor buffer
+           :initarg :buffer
+           :documentation "The wayland buffer made from the pool data")
+   ))
+
 (defclass ctx/wayland ()
   (;; Globals
    (wl-display :accessor wl-display)
@@ -29,57 +34,46 @@
    wl-surface
    xdg-surface
    xdg-toplevel
+   pool
    
    shm
-   pool-data
-   (front-buffer :accessor front-buffer :initform nil)
-   (back-buffer :accessor back-buffer)
-   back-buffer-size
-   (need-next-frame :accessor need-next-frame :initform nil)
+   (backing-pool-data :accessor backing-pool-data)
+   (backing-pool-data-size :accessor backing-pool-data-size)
+   
+   (front-buffer :accessor front-buffer :type render-buffer)
+   (back-buffer :accessor back-buffer :type render-buffer)
+   
+   (need-next-frame :accessor need-next-frame :initform t)
    (width :initform 640 :accessor width)
    (height :initform 480 :accessor height)
 
    ;; State
    (offset :initform 0.0)
    (last-frame :initform 0)
+   (configured :accessor configured :initform nil)
    (window-should-close :initform nil :accessor window-should-close))
   
   (:documentation "An example Wayland application"))
 
 (defmethod begin-drawing ((ctx ctx/wayland))
-  (with-slots (wl-shm width height shm pool-data back-buffer back-buffer-size) ctx
-    (let* ((stride (* width 4))
-           (size (* stride height))
-           )
-
-      ;;TODO creating the shm should be happen in init-window/wayland, not here
-      (setf shm (posix-shm:open-shm* :direction :io))
-            (posix-shm:truncate-shm shm size)
-      (setf pool-data (posix-shm:mmap-shm shm size))
-
-      
-      (with-proxy (pool (wl-shm.create-pool wl-shm (posix-shm:shm-fd shm) size))
-        (setf back-buffer (wl-shm-pool.create-buffer
-                           pool 0 width height stride
-                           :xrgb8888)))
-      (push (evelambda
-              (:release ()
-                        ;; Sent by the compositor when it's no longer using this buffer.
-                        (destroy-proxy back-buffer)))
-            (wl-proxy-hooks back-buffer))
-      )
-    )
   )
 
 (defmethod end-drawing ((ctx ctx/wayland))
-  ;; (posix-shm:close-shm shm)
-  ;; (posix-shm:munmap pool-data size)
+  (when (window-should-close ctx)
+    (return-from end-drawing))
+  
+  (unless (configured ctx)
+    (loop
+      (wl-display-dispatch-event (wl-display ctx))
+      (when (configured ctx) (return))))
 
   (loop :until (need-next-frame ctx)
-        :do (sleep 0.001)
+        :do ;; (format t "Waiting for next frame to be needed...~%")
+            (sleep 0.001)
             (wl-display-dispatch-event (wl-display ctx)))
+  (setf (need-next-frame ctx) nil)
   (with-slots (wl-surface) ctx
-    (let ((wl-buffer (back-buffer ctx)))
+    (let ((wl-buffer (buffer (back-buffer ctx))))
       (wl-surface.attach wl-surface wl-buffer 0 0)
       (wl-surface.damage-buffer
        wl-surface 0 0 +most-positive-wl-int+ +most-positive-wl-int+)
@@ -104,83 +98,21 @@
          )
     result))
 
-(defmethod draw-rectangle ((ctx ctx/wayland) x y width height color)
-  (with-slots (pool-data) ctx
-    (loop :for dx :from x :below (min width (width ctx)) :do
-      (loop :for dy :from y :below (min height (width ctx)) :do
-        (setf (cffi:mem-aref pool-data :uint32 (+ dx (* dy (width ctx))))
+(defmethod draw-rectangle ((ctx ctx/wayland) x y w h color)
+  (let* ((pool-data (pool-data (back-buffer ctx)))
+         (stride (* (width ctx) 4))        ; bytes per row
+         (row-pixels (/ stride 4))
+         (x-end (min (+ x w) (width ctx)))
+         (y-end (min (+ y h) (height ctx))))
+    (loop :for dy :from y :below y-end :do
+      (loop :for dx :from x :below x-end :do
+        (setf (cffi:mem-aref pool-data :uint32
+                              (+ dx (* dy row-pixels)))
               (color-to-xrbg color))))))
 
-(defun render-frame (app)
-  (with-slots (wl-shm offset) app
-    (let* ((width 640)
-           (height 480)
-           (stride (* width 4))
-           (size (* stride height))
-           (offset* (floor (mod offset 8)))
-           buffer
-           shm
-           pool-data
-           )
-      (setf shm (posix-shm:open-shm* :direction :io))
-      (posix-shm:truncate-shm shm size)
-      (setf pool-data (posix-shm:mmap-shm shm size))
-      (with-proxy (pool (wl-shm.create-pool wl-shm (posix-shm:shm-fd shm) size))
-        (setf buffer (wl-shm-pool.create-buffer
-                      pool 0 width height stride
-                      :xrgb8888)))
-      (dotimes (y height)
-        (dotimes (x width)
-          (setf (cffi:mem-aref pool-data :uint32 (+ (* y width) x))
-                (if (< (mod (+ (+ x offset*)
-                               (* (floor (+ y offset*) 8) 8)) 16)
-                       8)
-                    #xff666666
-                    #xffeeeeee))))
-      (push (evelambda
-              (:release ()
-                        ;; Sent by the compositor when it's no longer using this buffer.
-                        (destroy-proxy buffer)))
-            (wl-proxy-hooks buffer))
 
-      (posix-shm:close-shm shm)
-      (posix-shm:munmap pool-data size)
-      buffer
-      )
-    ))
-
-(defun draw-frame (app)
-  (with-slots (wl-shm offset) app
-    (let* ((width 640)
-           (height 480)
-           (stride (* width 4))
-           (size (* stride height))
-           (offset* (floor (mod offset 8)))
-           buffer)
-      (posix-shm:with-open-shm-and-mmap* (shm pool-data (:direction :io) (size))
-        (with-proxy (pool (wl-shm.create-pool wl-shm (posix-shm:shm-fd shm) size))
-          (setf buffer (wl-shm-pool.create-buffer
-                        pool 0 width height stride
-                        :xrgb8888)))
-
-        ;; Draw checkerboxed background
-        (dotimes (y height)
-          (dotimes (x width)
-            (setf (cffi:mem-aref pool-data :uint32 (+ (* y width) x))
-                  (if (< (mod (+ (+ x offset*)
-                                 (* (floor (+ y offset*) 8) 8)) 16)
-                         8)
-                      #xff666666
-                      #xffeeeeee))))
-
-        (push (evelambda
-                (:release ()
-                          ;; Sent by the compositor when it's no longer using this buffer.
-                          (destroy-proxy buffer)))
-              (wl-proxy-hooks buffer))
-        buffer))))
-
-(defun my/handle-frame-callback (ctx callback &rest event)
+(defun handle-frame-callback (ctx callback &rest event)
+  ;; (format t "Handing frame callback ~a~%" callback)
   (event-ecase event
     (:done (time)
            (declare (ignore time))
@@ -190,47 +122,11 @@
 
              ;; Request another frame
              (setf callback (wl-surface.frame wl-surface))
-             (push (alexandria:curry 'my/handle-frame-callback ctx callback)
+             (push (alexandria:curry 'handle-frame-callback ctx callback)
                    (wl-proxy-hooks callback))
 
              (setf (need-next-frame ctx) t)
-
-             ;; Update scroll amount at 24px/second
-
-             ;; Submit a new frame for this event
-             ;; (let ((wl-buffer (render-frame app)))
-             ;;   (wl-surface.attach wl-surface wl-buffer 0 0)
-             ;;   (wl-surface.damage-buffer
-             ;;     wl-surface 0 0 +most-positive-wl-int+ +most-positive-wl-int+)
-             ;;   (wl-surface.commit wl-surface))
-
-             ;; (setf last-frame time)
              ))))
-
-(defun handle-frame-callback (app callback &rest event)
-  (event-ecase event
-    (:done (time)
-           (with-slots (last-frame offset wl-surface) app
-             ;; Destroy this callback
-             (destroy-proxy callback)
-
-             ;; Request another frame
-             (setf callback (wl-surface.frame wl-surface))
-             (push (alexandria:curry 'handle-frame-callback app callback)
-                   (wl-proxy-hooks callback))
-
-             ;; Update scroll amount at 24px/second
-             (unless (zerop last-frame)
-               (incf offset (* (/ (- time last-frame) 1000.0) 24)))
-
-             ;; Submit a new frame for this event
-             (let ((wl-buffer (render-frame app)))
-               (wl-surface.attach wl-surface wl-buffer 0 0)
-               (wl-surface.damage-buffer
-                wl-surface 0 0 +most-positive-wl-int+ +most-positive-wl-int+)
-               (wl-surface.commit wl-surface))
-
-             (setf last-frame time)))))
 
 (defun handle-registry (app registry &rest event)
   (with-slots (wl-shm wl-compositor xdg-wm-base) app
@@ -257,21 +153,60 @@
                  (wl-proxy-hooks xdg-wm-base))))))))
 
 (defmethod close-window ((ctx ctx/wayland))
+  (with-slots (shm) ctx
+    (posix-shm:close-shm shm)
+    (posix-shm:munmap (backing-pool-data ctx) (backing-pool-data-size ctx)))
+
   (wl-display-disconnect (wl-display ctx)))
 
 (defun init-window/wayland (width height title)
-  (declare (ignore width height title))
-  (let ((app (make-instance 'ctx/wayland)))
+  (declare (ignore width height))
+  (let ((ctx (make-instance 'ctx/wayland)))
     (with-slots (wl-display wl-registry wl-shm wl-compositor
-                 xdg-wm-base wl-surface xdg-surface xdg-toplevel)
-        app
+                 xdg-wm-base wl-surface xdg-surface xdg-toplevel
+                 width height shm front-buffer back-buffer pool)
+        ctx
         
       ;; Register all globals
       (setf wl-display (wl-display-connect)
             wl-registry (wl-display.get-registry wl-display))
-      (push (alexandria:curry 'handle-registry app wl-registry)
+      (push (alexandria:curry 'handle-registry ctx wl-registry)
             (wl-proxy-hooks wl-registry))
       (wl-display-roundtrip wl-display)
+
+      ;; Allocate shm
+      (setf shm (posix-shm:open-shm* :direction :io))
+      (let* ((stride (* width 4))
+             (buffer-size (* height stride))
+             (total-size (* buffer-size 2))
+             )
+        (posix-shm:truncate-shm shm total-size)
+        (setf pool (wl-shm.create-pool wl-shm (posix-shm:shm-fd shm) total-size))
+        (setf (backing-pool-data ctx) (posix-shm:mmap-shm shm total-size))
+        (setf (backing-pool-data-size ctx) total-size)
+        (setf front-buffer
+              (make-instance 'render-buffer
+                             :pool-data (backing-pool-data ctx)
+                             :buffer (wl-shm-pool.create-buffer
+                                      pool 0 width height stride :xrgb8888)
+                             ))
+        (setf back-buffer
+              (make-instance 'render-buffer
+                             :pool-data (cffi:inc-pointer (backing-pool-data ctx) buffer-size)
+                             :buffer (wl-shm-pool.create-buffer
+                                      pool buffer-size width height stride :xrgb8888)
+                             ))
+         (push (evelambda
+                 (:release ()
+                           ))
+               (wl-proxy-hooks (buffer front-buffer)))
+
+         (push (evelambda
+                 (:release ()
+                           ))
+               (wl-proxy-hooks (buffer back-buffer)))
+        )
+
 
       ;; Create the surface & give it the toplevel role
       (setf wl-surface (wl-compositor.create-surface wl-compositor)
@@ -280,40 +215,47 @@
             xdg-toplevel (xdg-surface.get-toplevel xdg-surface))
       (push (evlambda
               (:close ()
-                      (setf (slot-value 'window-should-close app) t)))
+                      (setf (window-should-close ctx) t)))
             (wl-proxy-hooks xdg-toplevel))
       (push (evelambda
               (:configure (serial)
+                          (format t "configure received serial=~a~%" serial)
                           (xdg-surface.ack-configure xdg-surface serial)
-                          (let ((buffer (render-frame app)))
-                            (wl-surface.attach wl-surface buffer 0 0)
-                            (wl-surface.commit wl-surface))))
+                          (setf (configured ctx) t)
+                          ))
             (wl-proxy-hooks xdg-surface))
-      (xdg-toplevel.set-title xdg-toplevel "Example client")
+      (xdg-toplevel.set-title xdg-toplevel title)
       (wl-surface.commit wl-surface)
 
-      (let ((cb (wl-surface.frame wl-surface)))
-        (push (alexandria:curry 'my/handle-frame-callback app cb)
-              (wl-proxy-hooks cb)))
 
-      ;; (loop (wl-display-dispatch-event display))
-      (return-from init-window/wayland app)
+      (let ((cb (wl-surface.frame wl-surface)))
+        (push (alexandria:curry 'handle-frame-callback ctx cb)
+              (wl-proxy-hooks cb)))
+      (wl-display-roundtrip wl-display)
+      (return-from init-window/wayland ctx)
       )
     )
   )
 
-(defun run ()
-  (let ((app (init-window/wayland 100 100 "foo")))
-    (unwind-protect 
-         (loop :until (window-should-close app)
-               :do (wl-display-dispatch-event (wl-display app)))
-      (close-window app))
- ))
-
 (defun my/run ()
-  (let ((app (init-window/wayland 100 100 "foo")))
+  (let ((app (init-window/wayland 100 100 "foo"))
+        (x 10)
+        (y 35)
+        (dx 1)
+        (dy 1))
     (loop :until (window-should-close app) :do
           (begin-drawing app)
-          (draw-rectangle app 10 10 30 30 (make-color :r 10 :g 200 :b 20))
+          (draw-rectangle app 0 0 300 300 (make-color :r 130 :g 200 :b 220))
+          (draw-rectangle app x y 30 30 (make-color :r 10 :g 200 :b 20))
+          (when (> x 270)
+            (setf dx -1))
+          (when (< x 0)
+            (setf dx 1))
+          (when (> y 270)
+            (setf dy -1))
+          (when (< y 0)
+            (setf dy 1))
+          (setf x (+ x dx))
+          (setf y (+ y dy))
           (end-drawing app))
     (close-window app)))
