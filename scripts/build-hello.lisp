65b2b938ac8496e78139a00af5e6ea2ccb0d586b
Robert Burnett <dr.slate2@gmail.com>
2026-02-23 18:17:10 -0600

    create asdf setup script

--- scripts/build-hello.lisp
+++ scripts/build-hello.lisp
@@ -1,15 +1,8 @@
 ;;;; THIS IS A BUILD SCRIPT FOR THE REST OF THE PROJECT
 
-(declaim (optimize (speed 3) (safety 0)))
+(declaim (optimize (speed 3) (safety 3)))
 
-(unless (find-package 'asdf)
-  (load "./vendored/asdf/build/asdf.lisp"))
-
-(asdf:initialize-source-registry
- `(:source-registry
-   (:tree ,(uiop:getcwd))
-   :ignore-inherited-configuration))
-
-(asdf:load-system "clgfw")
+(load "setup.lisp")
+;; (asdf:load-system "clgfw")
 (asdf:make "clgfw/example/hello")
 (uiop:quit)
---
+++ scripts/setup.lisp
@@ -0,0 +1,10 @@
+(when (find-package 'cl-charms/low-level)
+  (delete-package 'cl-charms/low-level))
+
+(unless (find-package 'asdf)
+  (load "./vendored/asdf/build/asdf.lisp"))
+
+(asdf:initialize-source-registry
+ `(:source-registry
+   (:tree ,(uiop:getcwd))
+   :ignore-inherited-configuration))
