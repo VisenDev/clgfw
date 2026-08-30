a7661562c9d2668d20f84058b1e63a54763e310f
Robert Burnett <dr.slate2@gmail.com>
2026-08-30 14:32:30 -0500

    open a cocoa window with sbcl!!!

--- sb-cocoa.lisp
+++ sb-cocoa.lisp
@@ -15,19 +15,77 @@
 (define-alien-type id system-area-pointer)
 (define-alien-type Method system-area-pointer)
 (define-alien-type Ivar system-area-pointer)
+(define-alien-type NSPoint
+  (struct NSPoint
+    (x double)
+    (y double)))
+(define-alien-type NSSize
+  (struct NSSize
+    (width double)
+    (height double)))
+(define-alien-type NSRect
+  (struct NSRect
+    (origin (struct NSPoint))
+    (size   (struct NSSize))))
 
 (defun selector-get (selector-name)
   (alien-funcall
    (extern-alien "sel_registerName"
-                          (function SEL (c-string)))
+                 (function SEL c-string))
    (make-alien-string selector-name)))
 
 (defun class-get (class-name)
-  (alien-funcall (extern-alien "objc_getClass" (function Class (c-string)))
+  (alien-funcall (extern-alien "objc_getClass" (function Class c-string))
                  (make-alien-string class-name)))
 
-(defun message-send (id selector &rest args)
-;; OBJC_EXPORT id _Nullable
-;; objc_msgSend(id _Nullable self, SEL _Nonnull op, ...)
-;;    OBJC_AVAILABLE(10.0, 2.0, 9.0, 1.0, 2.0);
-  )
+
+(defmacro message-send (return-type id selector &rest arg-type-pairs)
+  `(alien-funcall (extern-alien
+                   "objc_msgSend"
+                   (function ,return-type
+                             id SEL ,@(mapcar
+                                       #'second
+                                       arg-type-pairs)))
+                  ,id ,selector ,@(mapcar #'first arg-type-pairs)))
+
+(defun main ()
+  (unless (sb-thread:main-thread-p)
+    (error "Cocoa requires running from the main thread"))
+  
+  (sb-int:with-float-traps-masked (:invalid :divide-by-zero)
+
+    (let* ((nsapp-class (class-get "NSApplication"))
+           (app (message-send
+                 id
+                 nsapp-class
+                 (selector-get "sharedApplication")))
+           
+           (nswindow-class (class-get "NSWindow"))
+           (window (message-send id nswindow-class (selector-get "alloc")))
+           (style 15))
+
+      (message-send boolean app
+                    (selector-get "setActivationPolicy:")
+                    (0 long))
+      (with-alien ((rect (struct NSRect)))
+        (setf (slot (slot rect 'origin) 'x) 100.0d0
+              (slot (slot rect 'origin) 'y) 100.0d0
+              (slot (slot rect 'size) 'width) 800.0d0
+              (slot (slot rect 'size) 'height) 600.0d0)
+        
+        (setf window
+              (message-send
+               id
+               window
+               (selector-get "initWithContentRect:styleMask:backing:defer:")
+               (rect (struct NSRect))
+               (style unsigned-long)
+               (2 unsigned-long)
+               (nil boolean))))
+
+      (message-send void window (selector-get "center"))
+      (message-send void window (selector-get "makeKeyAndOrderFront:")
+                    ((sb-sys:int-sap 0) id))
+      (message-send void app (selector-get "activateIgnoringOtherApps:")
+                    (t boolean))
+      (message-send void app (selector-get "run")))))
