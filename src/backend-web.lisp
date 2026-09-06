a345aa9a6d461f52743c8f1fe2bcbf7102d6c7ef
Robert Burnett <dr.slate2@gmail.com>
2026-09-06 16:17:41 -0500

    begin porting web backend to new backend api

--- src/backend-web.lisp
+++ src/backend-web.lisp
@@ -13,138 +13,140 @@
 ;;;; limitations under the License.
 
 
-(in-package #:clgfw)
+(defpackage #:clgfw/backend/web
+  (:use #:cl #:clgfw))
+(in-package #:clgfw/backend/web)
 
 #-jscl
 (error "The Web windowing backend requires JSCL")
 
 (defparameter *key-mapping-data*
-  '((:quote           "Quote")
-    (:comma           "Comma")
-    (:minus           "Minus")
-    (:period          "Period")
-    (:slash           "Slash")
+  '((key-quote           "Quote")
+    (key-comma           "Comma")
+    (key-minus           "Minus")
+    (key-period          "Period")
+    (key-slash           "Slash")
 
-    (:zero            "Digit0")
-    (:one             "Digit1")
-    (:two             "Digit2")
-    (:three           "Digit3")
-    (:four            "Digit4")
-    (:five            "Digit5")
-    (:six             "Digit6")
-    (:seven           "Digit7")
-    (:eight           "Digit8")
-    (:nine            "Digit9")
+    (key-zero            "Digit0")
+    (key-one             "Digit1")
+    (key-two             "Digit2")
+    (key-three           "Digit3")
+    (key-four            "Digit4")
+    (key-five            "Digit5")
+    (key-six             "Digit6")
+    (key-seven           "Digit7")
+    (key-eight           "Digit8")
+    (key-nine            "Digit9")
 
-    (:semicolon       "Semicolon")
-    (:equal           "Equal")
+    (key-semicolon       "Semicolon")
+    (key-equal           "Equal")
 
-    (:a               "KeyA")
-    (:b               "KeyB")
-    (:c               "KeyC")
-    (:d               "KeyD")
-    (:e               "KeyE")
-    (:f               "KeyF")
-    (:g               "KeyG")
-    (:h               "KeyH")
-    (:i               "KeyI")
-    (:j               "KeyJ")
-    (:k               "KeyK")
-    (:l               "KeyL")
-    (:m               "KeyM")
-    (:n               "KeyN")
-    (:o               "KeyO")
-    (:p               "KeyP")
-    (:q               "KeyQ")
-    (:r               "KeyR")
-    (:s               "KeyS")
-    (:t               "KeyT")
-    (:u               "KeyU")
-    (:v               "KeyV")
-    (:w               "KeyW")
-    (:x               "KeyX")
-    (:y               "KeyY")
-    (:z               "KeyZ")
+    (key-a               "KeyA")
+    (key-b               "KeyB")
+    (key-c               "KeyC")
+    (key-d               "KeyD")
+    (key-e               "KeyE")
+    (key-f               "KeyF")
+    (key-g               "KeyG")
+    (key-h               "KeyH")
+    (key-i               "KeyI")
+    (key-j               "KeyJ")
+    (key-k               "KeyK")
+    (key-l               "KeyL")
+    (key-m               "KeyM")
+    (key-n               "KeyN")
+    (key-o               "KeyO")
+    (key-p               "KeyP")
+    (key-q               "KeyQ")
+    (key-r               "KeyR")
+    (key-s               "KeyS")
+    (key-t               "KeyT")
+    (key-u               "KeyU")
+    (key-v               "KeyV")
+    (key-w               "KeyW")
+    (key-x               "KeyX")
+    (key-y               "KeyY")
+    (key-z               "KeyZ")
 
-    (:left-bracket    "BracketLeft")
-    (:backslash       "Backslash")
-    (:right-bracket   "BracketRight")
-    (:backtick        "Backquote")
+    (key-left-bracket    "BracketLeft")
+    (key-backslash       "Backslash")
+    (key-right-bracket   "BracketRight")
+    (key-backtick        "Backquote")
 
-    (:space           "Space")
-    (:escape          "Escape")
-    (:enter           "Enter")
-    (:tab             "Tab")
-    (:backspace       "Backspace")
-    (:insert          "Insert")
-    (:delete          "Delete")
+    (key-space           "Space")
+    (key-escape          "Escape")
+    (key-enter           "Enter")
+    (key-tab             "Tab")
+    (key-backspace       "Backspace")
+    (key-insert          "Insert")
+    (key-delete          "Delete")
 
-    (:right           "ArrowRight")
-    (:left            "ArrowLeft")
-    (:down            "ArrowDown")
-    (:up              "ArrowUp")
-    (:page-up         "PageUp")
-    (:page-down       "PageDown")
-    (:home            "Home")
-    (:end             "End")
+    (key-right           "ArrowRight")
+    (key-left            "ArrowLeft")
+    (key-down            "ArrowDown")
+    (key-up              "ArrowUp")
+    (key-page-up         "PageUp")
+    (key-page-down       "PageDown")
+    (key-home            "Home")
+    (key-end             "End")
 
-    (:caps-lock       "CapsLock")
-    (:scroll-lock     "ScrollLock")
-    (:num-lock        "NumLock")
-    (:print-screen    "PrintScreen")
-    (:pause           "Pause")
+    (key-caps-lock       "CapsLock")
+    (key-scroll-lock     "ScrollLock")
+    (key-num-lock        "NumLock")
+    (key-print-screen    "PrintScreen")
+    (key-pause           "Pause")
 
-    (:f1              "F1")
-    (:f2              "F2")
-    (:f3              "F3")
-    (:f4              "F4")
-    (:f5              "F5")
-    (:f6              "F6")
-    (:f7              "F7")
-    (:f8              "F8")
-    (:f9              "F9")
-    (:f10             "F10")
-    (:f11             "F11")
-    (:f12             "F12")
+    (key-f1              "F1")
+    (key-f2              "F2")
+    (key-f3              "F3")
+    (key-f4              "F4")
+    (key-f5              "F5")
+    (key-f6              "F6")
+    (key-f7              "F7")
+    (key-f8              "F8")
+    (key-f9              "F9")
+    (key-f10             "F10")
+    (key-f11             "F11")
+    (key-f12             "F12")
 
-    (:left-shift      "ShiftLeft")
-    (:right-shift     "ShiftRight")
+    (key-left-shift      "ShiftLeft")
+    (key-right-shift     "ShiftRight")
 
-    (:left-control    "ControlLeft")
-    (:right-control   "ControlRight")
+    (key-left-control    "ControlLeft")
+    (key-right-control   "ControlRight")
 
-    (:left-alt        "AltLeft")
-    (:right-alt       "AltRight")
+    (key-left-alt        "AltLeft")
+    (key-right-alt       "AltRight")
 
-    (:left-super      "MetaLeft")
-    (:right-super     "MetaRight")
+    (key-left-super      "MetaLeft")
+    (key-right-super     "MetaRight")
 
-    (:left-meta       "MetaLeft")
-    (:right-meta      "MetaRight")
+    (key-left-meta       "MetaLeft")
+    (key-right-meta      "MetaRight")
 
     ;; KeyboardEvent.code has no distinct Hyper code.
-    (:left-hyper    nil)
-    (:right-hyper   nil)
+    (key-left-hyper    nil)
+    (key-right-hyper   nil)
 
-    (:kb-menu         "ContextMenu")
+    (key-kb-menu         "ContextMenu")
 
-    (:keypad-0        "Numpad0")
-    (:keypad-1        "Numpad1")
-    (:keypad-2        "Numpad2")
-    (:keypad-3        "Numpad3")
-    (:keypad-4        "Numpad4")
-    (:keypad-5        "Numpad5")
-    (:keypad-6        "Numpad6")
-    (:keypad-7        "Numpad7")
-    (:keypad-8        "Numpad8")
-    (:keypad-9        "Numpad9")
-    (:keypad-decimal  "NumpadDecimal")
-    (:keypad-divide   "NumpadDivide")
-    (:keypad-multiply "NumpadMultiply")
-    (:keypad-subtract "NumpadSubtract")
-    (:keypad-add      "NumpadAdd")
-    (:keypad-enter    "NumpadEnter")
-    (:keypad-equal    "NumpadEqual")))
+    (key-keypad-0        "Numpad0")
+    (key-keypad-1        "Numpad1")
+    (key-keypad-2        "Numpad2")
+    (key-keypad-3        "Numpad3")
+    (key-keypad-4        "Numpad4")
+    (key-keypad-5        "Numpad5")
+    (key-keypad-6        "Numpad6")
+    (key-keypad-7        "Numpad7")
+    (key-keypad-8        "Numpad8")
+    (key-keypad-9        "Numpad9")
+    (key-keypad-decimal  "NumpadDecimal")
+    (key-keypad-divide   "NumpadDivide")
+    (key-keypad-multiply "NumpadMultiply")
+    (key-keypad-subtract "NumpadSubtract")
+    (key-keypad-add      "NumpadAdd")
+    (key-keypad-enter    "NumpadEnter")
+    (key-keypad-equal    "NumpadEqual")))
 
 (defparameter *js-key->lisp-key*
   (let ((tbl (make-hash-table :test 'equal)))
@@ -169,7 +171,7 @@
 ;;TODO support this
 ;; (defgeneric callback-on-window-resize (handler width height))
 
-(defmethod backend-init-window ((ctx backend/web) width height
+(defmethod %backend-init-window ((ctx backend/web) width height
                                 title callback-handler-instance)
   (setf (slot-value ctx 'callback-handler)
         callback-handler-instance)
@@ -192,26 +194,26 @@
 
   ;; register event handlers
   (flet ((on-mouse-move (e)
-           (callback-on-mouse-move callback-handler-instance
+           (%callback-on-mouse-move callback-handler-instance
                                    (jscl/ffi:oget e "clientX")
                                    (jscl/ffi:oget e "clientY")))
          (on-mouse-down (e)
-           (callback-on-mouse-down callback-handler-instance
+           (%callback-on-mouse-down callback-handler-instance
                                    (let ((btn (jscl/ffi:oget e "button")))
                                      (cond ((= btn 0) :left)
                                            ((= btn 1) :middle)
                                            ((= btn 2) :right)))))
          (on-mouse-up (e)
-           (callback-on-mouse-up callback-handler-instance
+           (%callback-on-mouse-up callback-handler-instance
                                    (let ((btn (jscl/ffi:oget e "button")))
                                      (cond ((= btn 0) :left)
                                            ((= btn 1) :middle)
                                            ((= btn 2) :right)))))
          (on-key-down (e)
-           (callback-on-key-down callback-handler-instance
+           (%callback-on-key-down callback-handler-instance
                                  (js-key->lisp-key (jscl/ffi:oget e "code"))))
          (on-key-up (e)
-           (callback-on-key-up callback-handler-instance
+           (%callback-on-key-up callback-handler-instance
                                (js-key->lisp-key (jscl/ffi:oget e "code")))))
 
     (#j:document:addEventListener #j"mousemove" #'on-mouse-move)
@@ -221,20 +223,20 @@
     (#j:document:addEventListener #j"keyup" #'on-key-up))
 
   ;; Notify of canvas size
-  (callback-on-window-resize callback-handler-instance width height)
+  (%callback-on-window-resize callback-handler-instance width height)
 
   ctx)
 
-(defmethod backend-close-window ((ctx backend/web))
-  ((jscl/ffi:oget (slot-value ctx 'canvas-node) "remove")))
+;; (defmethod backend-close-window ((ctx backend/web))
+;;   ((jscl/ffi:oget (slot-value ctx 'canvas-node) "remove")))
 
-(defmethod backend-begin-drawing ((ctx backend/web))
-  (error "Calling begin-drawing directly is not supported on the
-           web backend. Use with-drawing instead."))
+;; (defmethod backend-begin-drawing ((ctx backend/web))
+;;   (error "Calling begin-drawing directly is not supported on the
+;;            web backend. Use with-drawing instead."))
 
-(defmethod backend-end-drawing ((ctx backend/web))
-  (error "Calling end-drawing directly is not supported on the
-           web backend. Use with-drawing instead."))
+;; (defmethod backend-end-drawing ((ctx backend/web))
+;;   (error "Calling end-drawing directly is not supported on the
+;;            web backend. Use with-drawing instead."))
 
 (defun color->jsstring (color)
   (jscl/ffi:jsstring (format nil "rgba(~a, ~a, ~a, ~a)" 
@@ -243,25 +245,45 @@
                              (color-b color)
                              (color-a color))))
 
-(defmethod backend-draw-rectangle ((ctx backend/web) x y w h color)
-  (with-slots (canvas-ctx) ctx
-    (setf (jscl/ffi:oget canvas-ctx "fillStyle")
-          (color->jsstring color))
-    ((jscl/ffi:oget canvas-ctx "beginPath"))
-    ((jscl/ffi:oget canvas-ctx "rect") x y w h)
-    ((jscl/ffi:oget canvas-ctx "fill"))))
+(defmethod %backend-draw-rectangle (ctx x y w h color
+                                    &key angle (origin-x 0) (origin-y 0) target)
+  (let ((context-2d (if target
+                        ((jscl/ffi:oget target "getContext") #j"2d")
+                        (slot-value ctx 'canvas-ctx))))
 
-(defmethod backend-set-preferred-text-height ((ctx backend/web) text-height)
+    ;; begin path 
+    (setf (jscl/ffi:oget context-2d "fillStyle")
+          (color->jsstring color))
+    ((jscl/ffi:oget context-2d "beginPath"))
+    
+    ;; translate canvas and rotate when necessary, then draw rect
+    (cond ((and angle (not (= angle 0)))
+           ((jscl/ffi:oget context-2d "save"))
+           ((jscl/ffi:oget context-2d "translate")
+            (+ x origin-x)
+            (+ y origin-y))
+           ((jscl/ffi:oget context-2d "rotate")
+            (* angle #.(/ pi 180)))
+           ((jscl/ffi:oget context-2d "fillRect") 0 0 w h)
+           ((jscl/ffi:oget context-2d "restore")))
+
+          ;;else
+          (t ((jscl/ffi:oget context-2d "fillRect")
+              (+ x origin-x)
+              (+ y origin-y) w h)))))
+
+(defmethod %backend-set-preferred-text-height ((ctx backend/web) text-height)
   (setf (slot-value ctx 'text-height) text-height))
 
-(defmethod backend-get-text-height ((ctx backend/web))
+(defmethod %backend-get-text-height ((ctx backend/web))
   (slot-value ctx 'text-height))
 
-(defmethod backend-measure-text-width ((ctx backend/web) text)
+(defmethod %backend-measure-text-width ((ctx backend/web) text)
   (jscl/ffi:oget ((jscl/ffi:oget (canvas-ctx ctx) "measureText") text)
                  "width"))
 
-(defmethod backend-draw-text ((ctx backend/web) x y color text)
+;; TODO, continue porting from here
+(defmethod %backend-draw-text ((ctx backend/web) x y color text)
   (setf (jscl/ffi:oget (slot-value ctx 'canvas-ctx) "fillStyle")
         (color->jsstring color))
   (setf (jscl/ffi:oget (slot-value ctx 'canvas-ctx) "font")
